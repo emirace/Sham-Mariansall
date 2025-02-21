49a77ce522f9d2619e00bcfc1be5bd8c40939442
emirace <emmanuelakwuba57@gmail.com>
2025-02-21 05:35:47 +0100

    navbar

--- client/src/customer/components/navbar/NavData.jsx
+++ client/src/customer/components/navbar/NavData.jsx
@@ -1,13 +1,25 @@
 // Array containing navigation data for the client navbar
 const navData = [
   {
+    title: "home",
+    url: "/",
+  },
+  {
+    title: "services",
+    url: "/",
+  },
+  {
+    title: "portfolio",
+    url: "/",
+  },
+  {
     title: "stock",
     url: "/store",
   },
-  {
-    title: "how it works",
-    url: "/how-it-works",
-  },
+  // {
+  //   title: "how it works",
+  //   url: "/how-it-works",
+  // },
   {
     title: "Blog",
     url: "/blog",
